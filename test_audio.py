95c46139122c51f5e8d0029bca25ac11aa41ed5b
Adam Drew <ceifer35@gmail.com>
2013-12-27 12:31:05 -0800

    Made test_audio aware of OSX and RasberryPi

--- test_audio.py
+++ test_audio.py
@@ -1,8 +1,16 @@
 import subprocess
 import os
 
-print os.uname()
+audio_dir = 'audio'
+os_data = os.uname()
 
-subprocess.call(['afplay','one.m4a'])
-subprocess.call(['afplay','two.m4a'])
-subprocess.call(['afplay','three.m4a'])
+if os_data[1] == 'rasberrypi':
+    player = 'omxplayer'
+elif os_data[0] == 'Darwin':
+    player ='afplay'
+else:
+	raise StandardError('Unknown OS')
+
+subprocess.call([player,os.path.join(audio_dir,'one.m4a')])
+subprocess.call([player,os.path.join(audio_dir,'two.m4a')])
+subprocess.call([player,os.path.join(audio_dir,'three.m4a')])
