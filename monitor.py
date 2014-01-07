3c5bc872ba3f4924bb2d4e0c0810bc31a44fbc87
weakenedwarrior <ceifer35@gmail.com>
2014-01-07 00:37:46 -0800

    Added more tests to the Monitor class

--- monitor.py
+++ monitor.py
@@ -1,21 +1,49 @@
 
 class Monitor(object):
-    
-    
+
     def setSerial(self, SerialClass):
         self.ser = SerialClass()
         
+    def setThreshold(self, threshold):
+        self.threshold = threshold
+        
     def getPosList(self):
+        self.readline()
+        return self.poslist 
+    
+    def getClosedSensors(self):
+        self.readline()
+        return [i for i in range(len(self.poslist)) if 0 < self.poslist[i] <= self.threshold]
+    
+    def getOpenSensors(self):
+        self.readline()
+        return [i for i in range(len(self.poslist)) if 0 == self.poslist[i] or self.threshold < self.poslist[i]]   
+    
+    def getTriggeredSensor(self):
+        return self.getClosedSensors()[0]
+    
+    def sensorCount(self):
+        try:
+            return len(self.poslist)
+        except AttributeError:
+            raise SerialNotYetPolledError()    
+    
+    def readline(self):
         line = self.ser.readline()
         self.setPosList(line)
-        return self.poslist
     
     def setPosList(self, line):
-        poslist = []
+        self.poslist = []
         for elem in line.split(','):
             i, pos = elem.split(':')
-            poslist.append(int(pos))
-        self.poslist = poslist
-        
-
-        
+            self.poslist.append(int(pos))
+ 
+    def sensorCount(self):
+        try:
+            return len(self.poslist)
+        except AttributeError:
+            raise SerialNotYetPolledError()
+    
+    
+class SerialNotYetPolledError(StandardError):
+    pass
--- test_monitor.py
+++ test_monitor.py
@@ -1,20 +1,43 @@
 from unittest import TestCase
 
-from monitor import Monitor
+from monitor import Monitor, SerialNotYetPolledError
 
-SERIAL_LINE = '0:10,1:12,2:9,3:10,4:1'
-POS_LIST = [10,12,9,10,1]
+SERIAL_LINE = '0:10,1:12,2:9,3:0,4:1'
+POS_LIST = [10,12,9,0,1]
+THRESHOLD = 10
+CLOSED_SENSORS = [0,2,4]
+OPEN_SENSORS = [1,3]
 
 class test_monitor(TestCase):
     
     def setUp(self):
         self.mon = Monitor()
         self.mon.setSerial(MockSerial)
+        self.mon.setThreshold(THRESHOLD)
         
     def test_get_serial_data(self):
         self.assertEqual(self.mon.getPosList(),POS_LIST)
+        
+    def test_list_closed_sensors(self):
+        self.assertEqual(self.mon.getClosedSensors(),CLOSED_SENSORS)
+        
+    def test_list_open_sensors(self):
+        self.assertEqual(self.mon.getOpenSensors(),OPEN_SENSORS)
+            
+    def test_sensor_count_too_early_raises_exception(self):
+        self.assertRaises(SerialNotYetPolledError, self.mon.sensorCount)
+        
+    def test_list_sensor_count(self):
+        self.mon.readline()
+        self.assertEqual(len(POS_LIST), self.mon.sensorCount()) 
+        
+    def test_only_lists_one_triggered_sensor(self):
+        self.mon.readline()
+        self.assertEqual(self.mon.getTriggeredSensor(),CLOSED_SENSORS[0])
+        
+        
 
-
+    
 
     
 class MockSerial(object):
