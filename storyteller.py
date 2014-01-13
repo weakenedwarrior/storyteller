e337b200f55a4a2ef7b0c35c6adbbb2b095ac4c7
weakenedwarrior <ceifer35@gmail.com>
2014-01-13 07:32:23 -0800

    Added getting of the story lines

--- storyteller.py
+++ storyteller.py
@@ -1,22 +1,44 @@
+import os
+
 class StoryTeller(object):
     
     def __init__(self):
-        self.storynames = []
+        self.storylines = {}
         self.player = None
+        self.audiodir = None
     
     def play(self):
         if self.player == None:
             raise NoPlayerSet
         self.player.play()            
     
-    def listAll(self):
-        return self.storynames
-    
-    def add(self,storyname):
-        self.storynames.append(storyname)
+    def getStoryLines(self):
+        return self.storylines
         
     def setPlayer(self, player):
         self.player = player
+        
+    def setAudioDir(self, audiodir):
+        self.audiodir = audiodir
+        
+    def loadStoryLines(self):
+        for filename in os.listdir(self.audiodir):
+            if self.isValidAudioFile(filename):
+                self.appendStory(filename)
+        
+    def isValidAudioFile(self, filename):
+        return os.path.isfile(os.path.join(self.audiodir,filename))
+    
+    def appendStory(self,filename):
+        index = self.getIndex(filename)
+        if index in self.storylines:
+            self.storylines[index].append(filename)
+        else:
+            self.storylines[index] = [filename]
+            
+    def getIndex(self, filename):
+        return int(filename.split('_',1)[0])
+        
         
 class StoryTellerError(StandardError):
     pass
--- test_storyteller.py
+++ test_storyteller.py
@@ -1,21 +1,54 @@
 from unittest import TestCase
+import os
+import shutil
 
 from storyteller import StoryTeller, StoryTellerError
+
+AUDIODIR = 'audio'
+TESTDIR = 'test'
+
+AUDIOFILES = ['01_01_intro.wav',
+              '01_02_content.wav',
+              '02_01_intro.wav',
+              '02_02_content.wav',
+              '02_03_more_content.wav']
+
+STORYLENGTHS = {1:2,
+                2:3}
 
 class test_storyteller(TestCase):
     
     def setUp(self):
+        self.setUpStoryFiles()
         self.story = StoryTeller()
+        self.story.setAudioDir(TESTDIR)
+        self.story.loadStoryLines()
+        
+    def setUpStoryFiles(self):
+        for filename in AUDIOFILES:
+            self.touch(filename)
+        
+    def touch(self, filename):
+        if not os.path.exists(TESTDIR):
+            os.makedirs(TESTDIR) 
+        fullname = os.path.join(TESTDIR,filename)
+        with open(fullname, 'a'):
+            os.utime(fullname, None)   
+            
+    def tearDown(self): 
+        if os.path.exists(TESTDIR):
+            shutil.rmtree(TESTDIR)
         
     def test_story_teller_raise_error_if_no_player_installed(self):
         self.assertRaises(StoryTellerError,self.story.play)
         
     def test_can_list_stories(self):
-        self.assertIsInstance(self.story.listAll(),list)
-        
-    def test_can_set_a_story(self):
-        self.story.add('FirstStory')
-        self.assertTrue(len(self.story.listAll())==1)
+        storylines = self.story.getStoryLines()
+        self.assertIsInstance(storylines,dict)
+        count = len(STORYLENGTHS)
+        self.assertEqual(len(storylines), count)
+        for i in STORYLENGTHS:
+            self.assertEqual(len(storylines[i]), STORYLENGTHS[i])
         
     def test_can_set_the_story_player(self):
         self.story.setPlayer(MockPlayer)
