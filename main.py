e221a489acb8b80512fe05de701f3ad6643efce7
weakenedwarrior <ceifer35@gmail.com>
2014-01-19 19:56:42 -0800

    Added startup message

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ if __name__ == '__main__':
     s.loadStoryLines() 
     print s.showStoryLines()
        
+    s.playNext(10) # This is the startup message
+       
     print "Starting main loop..."
     
     while True:
